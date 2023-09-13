6550b6dbe970e77f0dd034fa37019c943c779a77
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-09-13 18:43:02 +0200

    implement header

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,48 @@
 import "./header.css";
+import HeaderImage from "../../assets/header.jpg";
+import data from "./data";
 
 const Header = () => {
   return (
     <header id="header">
-      <h2>Header</h2>
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Portait" />
+        </div>
+        <h3>Pinyada Sengate</h3>
+        <p>
+          With extensive experience in the field of game development and yes, I
+          have a residence card (NIE) from Spain that make me available to live
+          and work without any issue. I glad to offer to you my knowledge and
+          the ability to adapt to new technologies and methodologies to deliver
+          successful projects. I have also years working with international
+          teams who have made my communication skills good enough to understand
+          complex ideas and collaborate with colleagues from different
+          backgrounds. I am very excited to start working in Europe to show my
+          full potential.
+        </p>
+        <div className="header__cta">
+          <a href="#contact" className="btn primary">
+            Let's Talk
+          </a>
+          <a href="#portfolio" className="btn primary">
+            My Work
+          </a>
+        </div>
+
+        <div className="header__socials">
+          {data.map((item) => (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
     </header>
   );
 };
---
+++ src/sections/header/data.js
@@ -0,0 +1,25 @@
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineDribbble } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+
+const data = [
+  {
+    id: 1,
+    link: "https://github.com/pinyada-sengate",
+    icon: <AiOutlineInstagram />,
+  },
+  {
+    id: 2,
+    link: "https://github.com/pinyada-sengate",
+    icon: <AiOutlineTwitter />,
+  },
+  {
+    id: 3,
+    link: "https://github.com/pinyada-sengate",
+    icon: <AiOutlineDribbble />,
+  },
+  { id: 4, link: "https://github.com/pinyada-sengate", icon: <AiFillGithub /> },
+];
+
+export default data;
