21e86dd80bfbf1fad45b5c1db6a9ff48e9372157
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-09-14 16:45:27 +0200

    services section

---
+++ src/sections/services/data.js
@@ -0,0 +1,33 @@
+import { IoLogoGameControllerA } from "react-icons/io";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaServer } from "react-icons/fa";
+import { AiFillAppstore } from "react-icons/ai";
+
+const data = [
+  {
+    id: 1,
+    icon: <FaServer />,
+    title: "Backend Development",
+    desc: "3+ experience in backend development. ",
+  },
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "App Development",
+    desc: "I have applications runing on both Apple Store and Google Play Store.",
+  },
+  {
+    id: 3,
+    icon: <RiReactjsLine />,
+    title: "Frontend Development",
+    desc: "I have some experience in Frontend. For example Angular and Ruby on Rails. And I am currently learning React",
+  },
+  {
+    id: 4,
+    icon: <IoLogoGameControllerA />,
+    title: "Game Development",
+    desc: "6+ year in game development working in defferent technologies such as C++, C#, Lua, Unity",
+  },
+];
+
+export default data;
