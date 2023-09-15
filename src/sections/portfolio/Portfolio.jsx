04472065e5c44a54277ec7b358fe542494d28683
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-09-15 05:38:59 +0200

    portfolio categories

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -7,12 +7,30 @@ import { useState } from "react";
 const Portfolio = () => {
   const [projects, setProjects] = useState(data);
 
+  const categories = data.map((item) => item.category);
+  const uniqueCategories = ["all", ...new Set(categories)];
+
+  function filterProjectsHandler(category) {
+    if (category === "all") {
+      setProjects(data);
+      return;
+    }
+
+    const filterProjects = data.filter(
+      (project) => project.category === category
+    );
+    setProjects(filterProjects);
+  }
+
   return (
     <section id="portfolio">
       <h2>Recent Projects</h2>
       <p>Check out some of my projects that i recently worked on.</p>
       <div className="container portfolio__container">
-        <ProjectCategories />
+        <ProjectCategories
+          categories={uniqueCategories}
+          onFilterProjects={filterProjectsHandler}
+        />
         <Projects projects={projects} />
       </div>
     </section>
--- src/sections/portfolio/ProjectCategories.jsx
+++ src/sections/portfolio/ProjectCategories.jsx
@@ -1,5 +1,29 @@
-const ProjectCategories = () => {
-  return <div>ProjectCategories</div>;
+import { useState } from "react";
+import CategoryButton from "./CategoryButton";
+
+const ProjectCategories = ({ categories, onFilterProjects }) => {
+  const [activeCategory, setActiveCategory] = useState("all");
+
+  function changeCategoryHandler(category) {
+    console.log(category);
+    setActiveCategory(category);
+    onFilterProjects(category);
+  }
+
+  return (
+    <div className="portfolio__categories">
+      {categories.map((category) => (
+        <CategoryButton
+          key={category}
+          category={category}
+          onChangeCategory={() => changeCategoryHandler(category)}
+          className={`btn cat_btn ${
+            activeCategory === category ? "primary" : "white"
+          }`}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ProjectCategories;
