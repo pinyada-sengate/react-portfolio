4684856fa1f83c006b9bfa9c5bc251c79f9ebc5d
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-09-20 12:11:23 +0200

    added footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import "./footer.css";
+import { links, socials } from "./data";
 
 const Footer = () => {
   return (
     <footer>
-      <h2>Footer</h2>
+      <div className="container footer__container">
+        <ul className="nav__menu">
+          {links.map((flink) => (
+            <li key={flink.id}>
+              <a href={flink.link}>{flink.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="footer__socials">
+          {socials.map((social) => (
+            <a
+              key={social.id}
+              href={social.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {social.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <small>2023 Pinyada Sengate &copy; All rights Reserved</small>
+      </div>
     </footer>
   );
 };
