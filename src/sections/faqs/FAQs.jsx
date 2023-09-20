0e209ec82558d2c65d0ed4ce4cca7fae96de3a3e
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-09-20 09:03:28 +0200

    added faqs

--- src/sections/faqs/FAQs.jsx
+++ src/sections/faqs/FAQs.jsx
@@ -1,9 +1,20 @@
 import "./faqs.css";
+import data from "./data";
+import FAQ from "./FAQ";
 
 const FAQs = () => {
   return (
     <section id="faqs">
-      <h2>FAQs</h2>
+      <h2>Frequently Asked Questions</h2>
+      <p>
+        Here are some questions I usually get. if you still have more questions,
+        send me a message.
+      </p>
+      <div className="container faqs__container">
+        {data.map((faq) => (
+          <FAQ key={faq.id} faq={faq} />
+        ))}
+      </div>
     </section>
   );
 };
