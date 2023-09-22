f73a5a421734c27b1aed52ec9aa1d9f71b9d9780
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-09-22 17:59:06 +0200

    added floating nav

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,50 @@ import Portfolio from "./sections/portfolio/Portfolio";
 import Testimonials from "./sections/testimonials/Testimonials";
 import FAQs from "./sections/faqs/FAQs";
 import Contact from "./sections/contact/Contact";
-import FloatingNav from "./sections/floating-nav/FloatingNav";
+// import FloatingNav from "./sections/floating-nav/FloatingNav";
 import Footer from "./sections/footer/Footer";
 import Theme from "./theme/Theme";
 import { useThemeContext } from "./context/theme-context";
+// import { useRef, useState, useEffect } from "react";
 
 const App = () => {
   const { themeState } = useThemeContext();
+
+  // const mainRef = useRef();
+  // const [showFloatingNav, setShowFloatingNav] = useState(true);
+  // const [siteYPosition, setSiteYPosition] = useState(0);
+
+  // const showFloatingNavHandler = () => {
+  //   setShowFloatingNav(true);
+  // };
+
+  // const hideFloatingNavHandler = () => {
+  //   setShowFloatingNav(false);
+  // };
+
+  // // check if floating nav should be show or hide
+  // const floatingNavToggleHandler = () => {
+  //   // check if scrolled up or down at least 20 px
+  //   if (
+  //     siteYPosition < mainRef?.current?.getBoundingClientReact().y - 20 ||
+  //     siteYPosition > mainRef?.current?.getBoundingClientReact().y + 20
+  //   ) {
+  //     showFloatingNavHandler();
+  //     console.log("showFloatingNavHandler");
+  //   } else {
+  //     hideFloatingNavHandler();
+  //     console.log("hideFloatingNavHandler");
+  //   }
+
+  //   setSiteYPosition(mainRef?.current?.getBoundingClientReact().y);
+  // };
+
+  // useEffect(() => {
+  //   const checkYPosition = setInterval(floatingNavToggleHandler, 2000);
+
+  //   // cleanup function
+  //   return () => clearInterval(checkYPosition);
+  // }, [siteYPosition]);
 
   return (
     <main className={`${themeState.primary} ${themeState.background}`}>
@@ -26,6 +63,7 @@ const App = () => {
       <Contact />
       <Footer />
       <Theme />
+      {/* {showFloatingNav && <FloatingNav />} */}
       {/* <FloatingNav /> */}
     </main>
   );
