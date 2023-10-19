ed4cd5abc8eb852414998b6f94f93078c5ee15ee
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-10-19 17:18:53 +0200

    add my projects

--- src/sections/portfolio/Project.jsx
+++ src/sections/portfolio/Project.jsx
@@ -9,21 +9,23 @@ const Project = ({ project }) => {
       <h4>{project.title}</h4>
       <p>{project.desc}</p>
       <div className="portfolio__project-cta">
-        <a
-          href={project.demo}
-          className="btn sm"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Demo
-        </a>
+        {project.text1 && (
+          <a
+            href={project.demo}
+            className="btn sm"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {project.text1}
+          </a>
+        )}
         <a
           href={project.github}
           className="btn sm primary"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Github
+          {project.text2}
         </a>
       </div>
     </Card>
