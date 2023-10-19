41cd4ae56f95c18f775c999dde3c97a6de8e5460
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-10-19 18:18:10 +0200

    add my testimonails

--- src/sections/testimonials/data.js
+++ src/sections/testimonials/data.js
@@ -1,64 +1,72 @@
-import Avatar1 from '../../assets/avatar1.jpg'
-import Avatar2 from '../../assets/avatar2.jpg'
-import Avatar3 from '../../assets/avatar3.jpg'
-import Avatar4 from '../../assets/avatar4.jpg'
-import Avatar5 from '../../assets/avatar5.jpg'
-import Avatar6 from '../../assets/avatar6.jpg'
-import Avatar7 from '../../assets/avatar7.jpg'
-
+import Avatar1 from "../../assets/testimonials/alberto-munguía.webp";
+import Avatar2 from "../../assets/testimonials/gabriela-marie-ocreto.webp";
+import Avatar3 from "../../assets/testimonials/m.mudassir-manzoor.webp";
+import Avatar4 from "../../assets/testimonials/alexey-maisuradze.webp";
+import Avatar5 from "../../assets/testimonials/hazel.webp";
+import Avatar6 from "../../assets/testimonials/melinda.webp";
+import Avatar7 from "../../assets/testimonials/atrin-kiani.webp";
+import Avatar8 from "../../assets/testimonials/nanie-grace-arellano.webp";
 
 const data = [
-        {
-        id: 1,
-        quote: "It's truly amazing how you did exactly what I want with the little information I gave. You'll be my go-to developer from today. Highly recommend!",
-        avatar: Avatar1,
-        name: 'Nana Ama McBrown',
-        profession: 'Actress'
-        },
-        {
-        id: 2,
-        quote: "Good job, Hajia! Right on time! I really like how the project turned out, and my customers like it as well. It was fun collaborating with you.",
-        avatar: Avatar2,
-        name: 'Ernest Achiever',
-        profession: 'Web Developer'
-        },
-        {
-        id: 3,
-        quote: "Impressive work, buddy. Highly recommend!",
-        avatar: Avatar3,
-        name: 'Daniel Vinyo',
-        profession: 'Graphic Designer'
-        },
-        {
-        id: 4,
-        quote: "Wow. You finished earlier than you said. Everything's modern and beautifully done to the T. Great profession!",
-        avatar: Avatar4,
-        name: 'Diana Ayi',
-        profession: 'Singer'
-        },
-        {
-        id: 5,
-        quote: "I like how the app turned out. You are incredible, Hajia.",
-        avatar: Avatar5,
-        name: 'Vivian Mensah',
-        profession: 'Street Photographer'
-        },
-        {
-        id: 6,
-        quote: "Best developer in the world!",
-        avatar: Avatar6,
-        name: 'Hajia Bintu',
-        profession: 'Brand Influencer'
-        },
-        {
-        id: 7,
-        quote: "Keep doing great stuff, dear! Nicely executed!",
-        avatar: Avatar7,
-        name: 'Nicki Nyarko',
-        profession: 'Makeup Artist'
-        }
-    ]
+  {
+    id: 1,
+    quote:
+      "This app is perfect to learn hiragana, you can see the order in which you have to draw the strokes. The only thing that I'd like were added is an option to hear the sound of the caracter without have to select it again",
+    avatar: Avatar1,
+    name: "Alberto Munguía",
+    profession: "Actress",
+  },
+  {
+    id: 2,
+    quote:
+      "A friend of me mentioned its better to learn the alphabet and its strokes. This helps alot",
+    avatar: Avatar2,
+    name: "Gabriela Marie Ocreto (Gabbu)",
+    profession: "Web Developer",
+  },
+  {
+    id: 3,
+    quote: "Great 👍 application",
+    avatar: Avatar3,
+    name: "M.Mudassir Manzoor",
+    profession: "Graphic Designer",
+  },
+  {
+    id: 4,
+    quote: "Good app",
+    avatar: Avatar4,
+    name: "Alexey Maisuradze",
+    profession: "Singer",
+  },
+  {
+    id: 5,
+    quote:
+      "Very good way to learn hiragana, the font is thick though and makes it hard to read sometimes",
+    avatar: Avatar5,
+    name: "Hazel",
+    profession: "Street Photographer",
+  },
+  {
+    id: 6,
+    quote: "Excellent.. Thank you very much",
+    avatar: Avatar6,
+    name: "Melinda",
+    profession: "Brand Influencer",
+  },
+  {
+    id: 7,
+    quote: "GOOOOOD👊👊👊",
+    avatar: Avatar7,
+    name: "Atrin Kiani",
+    profession: "Makeup Artist",
+  },
+  {
+    id: 7,
+    quote: "it's Good apps ❤️",
+    avatar: Avatar8,
+    name: "Nanie Grace Arellano",
+    profession: "Makeup Artist",
+  },
+];
 
-
-
-    export default data
+export default data;
