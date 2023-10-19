cec8eabfe04c4b577e980511c68cc31225834554
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-10-19 19:03:08 +0200

    fixed typo in contact session

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <section id="contact">
       <h2>Get In Touch</h2>
-      <p>Send me a message via any og links below.</p>
+      <p>Send me a message via any of links below.</p>
       <div className="container contact__container" data-aos="fade-up">
         {data.map((contact) => (
           <a
