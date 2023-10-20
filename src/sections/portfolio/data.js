811506d74b7b66fd720189c45d6eb8121f172f9d
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-10-20 17:49:23 +0200

    added courses in porfolio section

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,18 +1,16 @@
-import Image1 from "../../assets/project1.jpg";
-import Image2 from "../../assets/project2.jpg";
-import Image3 from "../../assets/project3.jpg";
-import Image4 from "../../assets/project4.jpg";
-import Image5 from "../../assets/project5.jpg";
-import Image6 from "../../assets/project6.jpg";
-import Image7 from "../../assets/project7.jpg";
-import Image8 from "../../assets/project8.jpg";
-import Image9 from "../../assets/project9.jpg";
-
+// projects images
 import LockMeow from "../../assets/projects/lock-meow.png";
 import AgentDiver from "../../assets/projects/agent-driver.png";
 import RitualSomething from "../../assets/projects/ritual-something.png";
 import XpressDocuments from "../../assets/projects/xpress-documents.jpg";
 import LearnHiraganaKatakana from "../../assets/projects/learn-hiragana-katakana.png";
+
+// courses images
+import UdemyNodeJs from "../../assets/courses/udemy-nodejs-the-complete-guide.jpg";
+import UdemyWebDevelopment from "../../assets/courses/udemy-the-complete-web-development-bootcamp.jpg";
+import UdemyFlutterDevelopment from "../../assets/courses/udemy-the-complete-flutter-development-bootcamp.jpg";
+import UdemyGolang from "../../assets/courses/udemy-golang.jpg";
+import UdemyGolangRestAPIs from "../../assets/courses/udemy-golang-rest-apis.jpg";
 
 const data = [
   {
@@ -71,42 +69,64 @@ const data = [
     text1: "Android",
     text2: "iOS",
   },
-  // {
-  //   id: 6,
-  //   category: "frontend",
-  //   image: Image6,
-  //   title: "Project Title Six (Frontend)",
-  //   desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-  //   demo: "http://egatortutorials.com",
-  //   github: "https://github.com/egattor",
-  // },
-  // {
-  //   id: 7,
-  //   category: "frontend",
-  //   image: Image7,
-  //   title: "Project Title Seven (Frontend)",
-  //   desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-  //   demo: "http://egatortutorials.com",
-  //   github: "https://github.com/egattor",
-  // },
-  // {
-  //   id: 8,
-  //   category: "backend",
-  //   image: Image8,
-  //   title: "Project Title Eight (Backend)",
-  //   desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-  //   demo: "http://egatortutorials.com",
-  //   github: "https://github.com/egattor",
-  // },
-  // {
-  //   id: 9,
-  //   category: "frontend",
-  //   image: Image9,
-  //   title: "Project Title Nine (Frontend)",
-  //   desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-  //   demo: "http://egatortutorials.com",
-  //   github: "https://github.com/egattor",
-  // },
+  {
+    id: 6,
+    category: "courses",
+    image: UdemyNodeJs,
+    title: "NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)",
+    desc: "Master Node JS & Deno.js, build REST APIs with Node.js, GraphQL APIs, add Authentication, use MongoDB, SQL & much more!",
+    demo: "https://play.google.com/store/apps/details?id=com.pinyadapizza.japanesealphabet",
+    github:
+      "https://www.udemy.com/certificate/UC-99f6d165-622d-4caf-815e-969339414e51/",
+    text1: "",
+    text2: "Certificate",
+  },
+  {
+    id: 7,
+    category: "courses",
+    image: UdemyWebDevelopment,
+    title: "The Complete 2022 Web Development Bootcamp",
+    desc: "Become a Full-Stack Web Developer with just ONE course. HTML, CSS, Javascript, Node, React, MongoDB, Web3 and DApps",
+    demo: "https://play.google.com/store/apps/details?id=com.pinyadapizza.japanesealphabet",
+    github:
+      "https://www.udemy.com/certificate/UC-6f519bb9-7258-4b0b-bcc6-fb4962b6a260/",
+    text1: "",
+    text2: "Certificate",
+  },
+  {
+    id: 8,
+    category: "courses",
+    image: UdemyFlutterDevelopment,
+    title: "The Complete Flutter Development Bootcamp with Dart",
+    desc: "Officially created in collaboration with the Google Flutter team.",
+    demo: "https://play.google.com/store/apps/details?id=com.pinyadapizza.japanesealphabet",
+    github:
+      "https://www.udemy.com/certificate/UC-ca7190ca-a93e-4b4f-b806-693cb742a5d9/",
+    text1: "",
+    text2: "Certificate",
+  },
+  {
+    id: 9,
+    category: "courses",
+    image: UdemyGolang,
+    title: "Go: The Complete Developer's Guide (Golang)",
+    desc: "Master the fundamentals and advanced features of the Go Programming Language (Golang)",
+    demo: "https://play.google.com/store/apps/details?id=com.pinyadapizza.japanesealphabet",
+    github: "https://www.udemy.com/certificate/UC-YRAX9U0M/",
+    text1: "",
+    text2: "Certificate",
+  },
+  {
+    id: 10,
+    category: "courses",
+    image: UdemyGolangRestAPIs,
+    title: "Golang: Intro to REST APIs with Go programming lang (Golang)",
+    desc: "Golang: Intro to REST APIs with Golang (Go programming language)",
+    demo: "https://play.google.com/store/apps/details?id=com.pinyadapizza.japanesealphabet",
+    github: "https://www.udemy.com/certificate/UC-HL1XNUX6/",
+    text1: "",
+    text2: "Certificate",
+  },
 ];
 
 export default data;
