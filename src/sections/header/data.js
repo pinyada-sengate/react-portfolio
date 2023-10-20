a4a65567b2541b059757007100deb60c653c195d
Pinyada Sengate <pinyadasengate@Pinyadas-MacBook-Pro.local>
2023-10-20 15:14:38 +0200

    edited header added my socials

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,25 +1,32 @@
 import { AiOutlineInstagram } from "react-icons/ai";
-import { AiOutlineTwitter } from "react-icons/ai";
-import { AiOutlineDribbble } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
+import { AiFillFacebook } from "react-icons/ai";
+import { BsGooglePlay } from "react-icons/bs";
+import { GrAppleAppStore } from "react-icons/gr";
 
 const data = [
   {
     id: 1,
-    link: "https://github.com/pinyada-sengate",
+    link: "https://www.instagram.com/pinyadaadventures/",
     icon: <AiOutlineInstagram />,
   },
-  {
-    id: 2,
-    link: "https://github.com/pinyada-sengate",
-    icon: <AiOutlineTwitter />,
-  },
+  //   { id: 2, link: "https://twitter.com", icon: <AiOutlineTwitter /> },
+  { id: 2, link: "https://github.com/pinyada-sengate", icon: <AiFillGithub /> },
   {
     id: 3,
-    link: "https://github.com/pinyada-sengate",
-    icon: <AiOutlineDribbble />,
+    link: "https://www.facebook.com/pinyada.sengate",
+    icon: <AiFillFacebook />,
   },
-  { id: 4, link: "https://github.com/pinyada-sengate", icon: <AiFillGithub /> },
+  {
+    id: 4,
+    link: "https://play.google.com/store/apps/dev?id=5239894305954758080",
+    icon: <BsGooglePlay />,
+  },
+  {
+    id: 5,
+    link: "https://apps.apple.com/us/developer/pinyada-sengate/id1244402942",
+    icon: <GrAppleAppStore />,
+  },
 ];
 
 export default data;
